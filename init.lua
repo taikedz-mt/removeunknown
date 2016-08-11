d4c542df548ff40eada624bbe820e55c38c3e44b
Tai @ Flex <dch.tai@gmail.com>
2016-08-11 17:10:55 +0100

    corrected mod_path fetch

--- init.lua
+++ init.lua
@@ -1,9 +1,13 @@
+--uzapper = {}
+
 -- Define tables here
 
-dofile( minetest.modpath("uzapper")..'replacement_table.lua' )
-dofile( minetest.modpath("uzapper")..'replacement.lua' )
+dofile( minetest.get_modpath("uzapper")..'/replacement_table.lua' )
+dofile( minetest.get_modpath("uzapper")..'/replacement.lua' )
 
-dofile( minetest.modpath("uzapper")..'removal_table.lua' )
-dofile( minetest.modpath("uzapper")..'removal.lua' )
+dofile( minetest.get_modpath("uzapper")..'/removal_table.lua' )
+dofile( minetest.get_modpath("uzapper")..'/removal.lua' )
 
-dofile( minetest.modpath("uzapper")..'zaptool.lua' ) -- use the replacement and removal tables
+dofile( minetest.get_modpath("uzapper")..'/zaptool.lua' ) -- use the replacement and removal tables
+
+minetest.chat_send_all("Uzapper loaded")
