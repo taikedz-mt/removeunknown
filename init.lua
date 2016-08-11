a3ecdd42da92573129e0e860c06f4209cc5c03a5
Tai @ Flex <dch.tai@gmail.com>
2016-08-11 13:24:52 +0100

    cannot automate this way - mods do not declare explicitly in predictable manner

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 -- Define tables here
 
-dofile( minetest.modpath("uzapper")..'replacement.lua' )
+dofile( minetest.modpath("uzapper")..'replacement_table.lua' )
 dofile( minetest.modpath("uzapper")..'replacement.lua' )
 
-dofile( minetest.modpath("uzapper")..'removal.lua' )
+dofile( minetest.modpath("uzapper")..'removal_table.lua' )
 dofile( minetest.modpath("uzapper")..'removal.lua' )
 
 dofile( minetest.modpath("uzapper")..'zaptool.lua' ) -- use the replacement and removal tables
